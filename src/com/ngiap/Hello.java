d704a064ee084e7476034b0d668bb8d2c9a278a7
steve <steven.yong@comptel.com>
2011-02-10 14:51:54 +0800

    add dialog

--- src/com/ngiap/Hello.java
+++ src/com/ngiap/Hello.java
@@ -3,9 +3,15 @@ package com.ngiap;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MenuInflater;
+import android.app.Dialog;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 public class Hello extends Activity {
+    
+    private static final int DIALOG_ONE = 1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,8 +25,49 @@ public class Hello extends Activity {
         return true;
     }
     
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+        case R.id.eight:
+            showDialog(DIALOG_ONE);
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }    
+    
+    protected Dialog onCreateDialog(int id) {
+        Dialog dialog;
+        switch(id) {
+        case DIALOG_ONE:
+            dialog = createAlertDialog();
+            break;
+        default:
+            dialog = null;
+        }
+        return dialog;
+    }    
+    
 	@SuppressWarnings("unused")
 	private static void walk() {
     	System.out.println("walk");
+    }
+    
+    private Dialog createAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to exit?")
+               .setCancelable(false)
+               .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                   }
+               })
+               .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                   }
+               });
+        return builder.create();
     }
 }
